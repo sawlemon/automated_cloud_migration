972c265dbbca3dc82f42774be0abfe455b9d8417
salakhaliff <salakhaliffjr@outlook.com>
2021-03-13 18:54:29 +0530

    files modified

--- pre-requisites/check_apache_directory.sh
+++ pre-requisites/check_apache_directory.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
+# Root User Check
 if [ "$(id -u)" != "0" ]; then
         echo "This script must run using root user"
         exit 1
 fi
 
-
+# Determind The Linux Distribution running on 
+# Categorises RHEL or Debian 
 if [ -f /etc/redhat-release ]; then
         echo "redhat detected"
         if [ `sudo httpd -v > /dev/null && echo $? -eq "0"` ]; then
                 echo "Apache found"
                 else
-                        echo "Server not found"
+                        echo "Apache Not Installed"
                         exit 1
         fi
+        # could be more optimzed. ser a variable is_instlled = true if installed, and then use a single condition to exit
         elif [ -f /etc/debian_version ]; then
                 echo "Debian based os"
                 if [ `sudo apache2 -v > /dev/null && echo $? -eq "0"` ]; then
                         echo "Apache found"
                 else
-                        echo "Server not found"
+                        echo "Apache Not Installed"
                         exit 1
 fi
 
+# Checks whether index.html is present
 if [ ! -f /var/www/index.html ]; then
-        echo "File not found"
-        exit 1
+    echo "HTML index file not found"
+    exit 1
 fi
 
+# Cheks whether aws cli is installed, if not instlls
 if [ `aws -V > /dev/null && echo $? -eq "0"` ]; then
     echo "aws found"
-
     else
-          curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-          unzip awscliv2.zip
-          sudo ./aws/install
-          echo "awscli install successfully"
+        sudo apt install curl
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
+        sudo ./aws/install
+        echo "awscli install successfully"
 fi
--- root_user_or_not.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-if [ "$(id -u)" != "0" ]; then
-	echo "Your not a root user"
-else
-	echo "Your are a root user"
-fi
