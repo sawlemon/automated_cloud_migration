e9638fac7d81bf22fd89626f833eb2c866613097
salakhaliff <salakhaliffjr@outlook.com>
2021-03-13 19:34:05 +0530

    configure user

--- aws-account-setup/configure-user.sh
+++ aws-account-setup/configure-user.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-
 # Cheks whether aws cli is installed, if not instlls
 # aws --help >> /dev/null
 if [ `sudo aws --help > /dev/null && echo $? -eq 0` ]; 
 then
     echo "aws found"
 else
-    echo "aws not found"
-    # sudo apt install curl
-    # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    # unzip awscliv2.zip
-    # sudo ./aws/install
-    # echo "aws-cli install successfully"
-    # rm  -rf awscliv2.zip aws
+    echo "aws-cli not found\nInstalling aws-cli..........................................."
+    sudo apt install -y curl unzip
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    echo "aws-cli install successfully"
+    sudo rm  -rf awscliv2.zip aws
 fi
 
